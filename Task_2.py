764ee8bd684e061e6dec0cd623e9f634b61602ab
highgroll <42375044+highgroll@users.noreply.github.com>
2020-04-30 14:55:21 +0300

    Delete Task_2.py

--- Task_2.py
+++
@@ -1,7 +0,0 @@
-time_sec = int(input("Введите время в секундах: "))
-
-hours = time_sec // 3600
-minutes = (time_sec - (hours * 3600)) // 60
-seconds = (time_sec - (hours * 3600)) - (minutes * 60)
-print("{:02}:{:02}:{:02}".format(hours, minutes, seconds))
-
